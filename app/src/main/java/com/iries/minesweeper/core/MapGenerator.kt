cfd78302a5a70e51efca04324e8d037eeaf6cdb5
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-04-10 14:00:16 +0800

    Added error check when player's start point is out of bounds.

--- app/src/main/java/com/iries/minesweeper/core/MapGenerator.kt
+++ app/src/main/java/com/iries/minesweeper/core/MapGenerator.kt
@@ -2,25 +2,28 @@ package com.iries.minesweeper.core
 
 import kotlin.random.Random
 
-fun generateMap(startPoint: Point, mapSize: Point, minesNum: Int): ArrayList<Point>? {
+fun generateMap(startPoint: Point, mapBounds: Point, minesNum: Int): ArrayList<Point>? {
 
     // Handling special cases where map generation is impossible
-    val map = mapSize.x * mapSize.y
-    if (map < 2) {
+    val mapSize = mapBounds.x * mapBounds.y
+    if (mapSize < 2) {
         println("Map size can't be less than 2. Please, enter correct map size.")
         return null
     } else if (minesNum < 1) {
         println("Mines number can't be below 1. Please, enter correct amount of mines.")
         return null
+    } else if (startPoint.x >= mapBounds.x || startPoint.y >= mapBounds.y) {
+        println("Start point is out of bounds. Please, enter correct start point.")
+        return null
     }
 
-    val maxSafeArea = map - minesNum
+    val maxSafeArea = mapSize - minesNum
     var checkedCellNum = 0
     var currentMinesNum = 0
     val minesList: ArrayList<Point> = ArrayList()
 
-    mapLoop@ for (i in 0..<mapSize.y) { // for each map's column
-        for (j in 0..<mapSize.x) { // for each colum's row
+    mapLoop@ for (i in 0..<mapBounds.y) { // for each map's column
+        for (j in 0..<mapBounds.x) { // for each colum's row
             if (currentMinesNum == minesNum) {
                 println("All mines are generated")
                 break@mapLoop
@@ -28,7 +31,7 @@ fun generateMap(startPoint: Point, mapSize: Point, minesNum: Int): ArrayList<Poi
             checkedCellNum++
             if (!(startPoint.y == i && startPoint.x == j)) {
                 // if safe area size is maxed or it's a mine cell
-                if (checkedCellNum >=maxSafeArea || Random.nextBoolean()) {
+                if (checkedCellNum >= maxSafeArea || Random.nextBoolean()) {
                     currentMinesNum++
                     minesList.add(Point(j, i))
                     println("Mine ($j, $i)")
@@ -37,9 +40,9 @@ fun generateMap(startPoint: Point, mapSize: Point, minesNum: Int): ArrayList<Poi
         }
     }
 
-    for (i in 0..mapSize.y) { // for each map's column
+    for (i in 0..mapBounds.y) { // for each map's column
         println(" ")
-        for (j in 0..<mapSize.x) {
+        for (j in 0..<mapBounds.x) {
             if (minesList.any { p -> p.x == j && p.y == i }) print(" Mine ")
             else {
                 if (startPoint.x == j && startPoint.y == i)
