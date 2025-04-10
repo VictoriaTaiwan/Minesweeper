3bc366dfa1b9df03a177f42d867d52f7c6786ea8
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-04-10 20:44:44 +0800

    Commented exceptions handling.

--- app/src/main/java/com/iries/minesweeper/core/BoardGenerator.kt
+++ app/src/main/java/com/iries/minesweeper/core/BoardGenerator.kt
@@ -7,6 +7,7 @@ private const val minMinesNumber = 1
 
 fun generateBoard(rows: Int, columns: Int): Board {
     val boardSize = rows * columns
+    // Check that board size isn't less than 2
     if (boardSize < minBoardSize) {
         throw (Exception(
             "Map size can't be less than 2. Please, enter correct map size."
@@ -17,12 +18,22 @@ fun generateBoard(rows: Int, columns: Int): Board {
 }
 
 fun generateMines(startPoint: Point, board: Board, minesNum: Int): ArrayList<Point> {
+    val boardSize = board.rows * board.columns
+    // Check that board size isn't less than 2
+    if (boardSize < minBoardSize) {
+        throw (Exception(
+            "Map size can't be less than 2. Please, enter correct map size."
+        ))
+    }
+    // Check that mines number isn't less than 1 and doesn't exceed board's size.
     if (minesNum < minMinesNumber || minesNum >= board.rows * board.columns) {
         throw (Exception(
             "Mines number can't be below 1, bigger than board size or equals to it. " +
                     "Please, enter correct amount of mines."
         ))
-    } else if (startPoint.x >= board.rows || startPoint.y >= board.columns
+    }
+    // Check that the start point doesn't exceed board's size and isn't negative.
+    if (startPoint.x >= board.rows || startPoint.y >= board.columns
         || startPoint.x < 0 || startPoint.y < 0
     ) {
         throw (Exception("Start point is out of bounds. Please, enter correct start point."))
@@ -31,8 +42,7 @@ fun generateMines(startPoint: Point, board: Board, minesNum: Int): ArrayList<Poi
     val minesList: ArrayList<Point> = ArrayList()
     val potentialMinePoints: ArrayList<Point> = ArrayList()
 
-    // populate list of all points except the starting point
-    // as potential mine slots
+    // Populate list of all points except the starting point as potential mine slots
     for (i in 0..<board.columns) { // for each board's column
         for (j in 0..<board.rows) { // for each board's row
             if (!(startPoint.x == j && startPoint.y == i)) // skip the start point
@@ -44,9 +54,9 @@ fun generateMines(startPoint: Point, board: Board, minesNum: Int): ArrayList<Poi
     for (i in 0..<minesNum) {
         var point: Point?
         val index = Random.nextInt(0, potentialMinePoints.size) // get a random index
-            point = potentialMinePoints[index]
-            potentialMinePoints.remove(point) // remove the point from potential mines list
-            minesList.add(point) // add the point to the actual mines list
+        point = potentialMinePoints[index]
+        potentialMinePoints.remove(point) // remove the point from potential mines list
+        minesList.add(point) // add the point to the actual mines list
     }
 
     return minesList
